b20c873b1f246280e80d28b4a7e67132f0bccbbb
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-11-10 15:44:38 +0300

    :hammer: final

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Disney+ clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
